d85223da9c0ee6951b3ca39e3342686ab0cf009a
iibadreeva <iibadreeva@mail.ru>
2021-01-12 21:29:21 +0300

    dd

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,39 @@
-import './sass/app.scss';
-import router from './router';
-import { UserAPI } from './core/modules/http/user-api';
-import { Page404 } from './pages/404/404';
-import { Page500 } from './pages/500/500';
-import { Login } from './pages/login/login';
-import { Registration } from './pages/registration/registration';
-import { Chat } from './pages/chat/chat';
-import { Profile } from './pages/profile/profile';
-import { ProfileChange } from './pages/profile_change/profile_change';
-import { ProfilePassword } from './pages/profile_password/profile_password';
+import "./sass/app.scss";
+import router from "./router";
+import { UserAPI } from "./core/modules/http/user-api";
+import { Page404 } from "./pages/404/404";
+import { Page500 } from "./pages/500/500";
+import { Login } from "./pages/login/login";
+import { Registration } from "./pages/registration/registration";
+import { Chat } from "./pages/chat/chat";
+import { Profile } from "./pages/profile/profile";
+import { ProfileChange } from "./pages/profile_change/profile_change";
+import { ProfilePassword } from "./pages/profile_password/profile_password";
 
 ssdsdss;
 sf;
 
 router
-  .useProtect('/chat', Chat)
-  .useProtect('/profile', Profile)
-  .useProtect('/change', ProfileChange)
-  .useProtect('/password', ProfilePassword)
-  .use('/404', Page404)
-  .use('/500', Page500);
+  .useProtect("/chat", Chat)
+  .useProtect("/profile", Profile)
+  .useProtect("/change", ProfileChange)
+  .useProtect("/password", ProfilePassword)
+  .use("/404", Page404)
+  .use("/500", Page500);
 
 new UserAPI()
   .request()
   .then((res) => res.ok)
   .then((isAuth) => {
     if (isAuth) {
-      router.use('/login', Login);
-      router.use('/registration', Registration);
+      router.use("/login", Login);
+      router.use("/registration", Registration);
 
       router.isProtect = false;
       router.start();
     } else {
-      router.useDefault('/login', Login);
-      router.use('/registration', Registration);
+      router.useDefault("/login", Login);
+      router.use("/registration", Registration);
       router.start();
     }
   });
