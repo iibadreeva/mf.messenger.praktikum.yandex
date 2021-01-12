b5f43d6960bd767a1cd4cb6b498a6310e016d4ef
iibadreeva <iibadreeva@mail.ru>
2021-01-12 14:51:49 +0300

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,116 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+
+const filename = ext => isDev ? `main.${ext}` : `main.[hash:8].${ext}`
+
+module.exports = {
+  entry: { main: './src/index.ts' },
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: filename('js'),
+  },
+  devtool: isDev ? 'source-map' : false,
+  resolve: {
+    extensions: ['.ts', '.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve(__dirname, 'src/core')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              configFile: path.resolve(__dirname, 'tsconfig.json'),
+            },
+          }
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/',
+              useRelativePath: true
+            },
+          },
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              optipng: {
+                enabled: false,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          { loader: 'sass-loader' }
+        ]
+      },
+      {
+        test: /\.ico$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              esModule: false,
+              name: "[name].[ext]",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: filename('css'),
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      minify: {
+        removeComments: !isDev,
+        collapseWhitespace: !isDev
+      }
+    }),
+  ],
+  devServer: {
+    //contentBase: path.join(__dirname, 'build'),
+    inline: true,
+    hot: isDev,
+    port: 9000,
+    //writeToDisk: true,
+    historyApiFallback: true,
+  }
+};
+
