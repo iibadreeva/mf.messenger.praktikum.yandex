048b87a87ba47f99888d2fa20760fa57b16b0dfe
iibadreeva <iibadreeva@mail.ru>
2021-01-22 03:30:30 +0300

    server file

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const limiter = require('express-rate-limit');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(
   limiter({
     windowMs: 3000,
     max: 300,
-    message: 'Слишком много запросов с одного IP'
+    message: 'Слишком много запросов с одного IP',
   })
 );
 
 app.use(express.static(`${__dirname}/build`));
-app.get('*/',function(req, res){
+app.get('*/', function (req, res) {
   res.sendfile(__dirname + '/build/index.html');
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`Server access: http://localhost:${PORT}/`);
 });
